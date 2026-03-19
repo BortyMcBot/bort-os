ed14c31b8aa213587bac52f7acb09c59348aece8
BortyMcBot <bort@ballersanonymo.us>
2026-03-19 16:31:21 +0000

    fix: [SELF-REVIEW] latestCommitSubjects() calls execSync(...) but only

--- os/x_daily_post.js
+++ os/x_daily_post.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const path = require('path');
-const { execFileSync } = require('child_process');
+const { execFileSync, execSync } = require('child_process');
 const { xCall } = require('./x_call');
 const { TELEGRAM_CHAT_ID } = require('./constants');
 
